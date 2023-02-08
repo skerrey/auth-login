bdbf3495aaab939c9b266da170ba508b17e671ab
skerrey <sethkerrey@gmail.com>
2023-02-07 20:59:36 -0600

    Delete import

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,7 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-import React from 'react'
-
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0(); // auth0 hook
   return (
