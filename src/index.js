bb171c169289a255d8ba32661d5107b0ad10896a
skerrey <sethkerrey@gmail.com>
2023-02-07 20:59:51 -0600

    Update redirectUri

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ root.render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}  
+      // redirectUri={window.location.origin}  
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
     >
       <App />
     </Auth0Provider>
-  
   </React.StrictMode>
 );
